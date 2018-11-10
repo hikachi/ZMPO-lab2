c33813e00f331a905a0b277cd6d5ebe88664dbb8
Hikachi <hikachi420@gmail.com>
2018-11-10 18:43:39 +0100

    v 1.3.3

--- ZMPO lab2/ZMPO lab2/CMenu.cpp
+++ ZMPO lab2/ZMPO lab2/CMenu.cpp
@@ -29,18 +29,18 @@ void showMenu() {
 	
 
 	for (int x = 0;x<list.size() ; x++) {
-		cout << menu[x]->getName() << "(type: " << menu[x]->getCommand() << " )" << endl;
+		cout << list[x]->getName() << "(type: " << list[x]->getCommand() << " )" << endl;
 	}
 }
 void CMenu::initalizeMainMenu()
 {
 	CTestClass *command1 = new CTestClass("test1", "test1");
 	CMenuCommand *op1 = new CMenuCommand(command1);
-	menu.push_back(op1);
+	list.push_back(op1);
 
 	CTestClass1 *command2 = new CTestClass1("test2", "test2");
 	CMenuCommand *op2 = new CMenuCommand(command2);
-	menu.push_back(op2);
+	list.push_back(op2);
 
 }
 
